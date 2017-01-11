80fa0c0176bc441b05d2b6a5e33d0e35633131fe
Pap Lőrinc <paplorinc@gmail.com>
2017-01-11 12:35:23 +0200

    Changed character replacement to lowercase conversion

--- src/main/java/pap/lorinc/TextEditor.java
+++ src/main/java/pap/lorinc/TextEditor.java
@@ -7,13 +7,14 @@ import org.openjdk.jol.info.GraphLayout;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import static java.lang.Character.*;
 import static java.lang.String.format;
 import static javaslang.API.List;
 import static javaslang.collection.Iterator.continually;
 
+@SuppressWarnings("ConstantConditions")
 public class TextEditor {
     private static final int SIZE = 1_000;
-    private static final char REPLACEMENT = '.';
     static final String STRING_TEXT = continually(TextEditor::randomChar).take(SIZE).mkString();
     static final Vector<Character> VECTOR_TEXT = Vector.ofAll(STRING_TEXT.toCharArray());
 
@@ -29,11 +30,11 @@ public class TextEditor {
 
     static Seq<String> editString(String text) {
         return editAllChars(text, text.length(),
-                (t, i) -> t.substring(0, i) + REPLACEMENT + t.substring(i + 1));
+                (t, i) -> t.substring(0, i) + toLowerCase(t.charAt(i)) + t.substring(i + 1));
     }
     static Seq<Vector<Character>> editVector(Vector<Character> text) {
         return editAllChars(text, text.length(),
-                (t, i) -> t.update(i, REPLACEMENT));
+                (t, i) -> t.update(i, toLowerCase(t.get(i))));
     }
 
     private static <T> Seq<T> editAllChars(T text, int length, Function2<T, Integer, T> replacer) {
